db7a6cea709019a8a6d0754019a36ac781906202
Zteid7464 <thokabafi69@gmain.com>
2025-03-29 13:40:03 -0600

    basic setup

---
+++ src/PDCode.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "../stb_image/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../stb_image/stb_image_write.h" 
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
