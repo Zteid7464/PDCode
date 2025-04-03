5df541dc9bf9cb0716150b51654e713793f7dbc7
Zteid7464 <thokabafi69@gmain.com>
2025-04-03 14:18:04 -0600

    Added basic functionality to the readPDC function

--- src/PDCode.c
+++ src/PDCode.c
@@ -20,29 +20,36 @@ int readPDC(int point_size, unsigned int distance_count, unsigned int* distances
 
 int main(int argc, char* argv[]) {
     int testWidth = 1000, testHeight = 1000;
-    uint8_t* testData;
+    uint8_t* testImg;
 
-    testData = (uint8_t*)calloc(testWidth*testHeight*3, 1);
+    testImg = (uint8_t*)calloc(testWidth*testHeight*3, 1);
 
     for (int y = 0; y < testHeight; y++) {
         for (int x = 0; x < testWidth; x++) {
             int pos = (y*testWidth + x) * 3;
 
-            *(testData + pos + 0) = 255;
-            *(testData + pos + 1) = 255;
-            *(testData + pos + 2) = 255;
+            *(testImg + pos + 0) = 255;
+            *(testImg + pos + 1) = 255;
+            *(testImg + pos + 2) = 255;
         
         }
     }
 
     unsigned int distances[] = {7, 17};
-    uint8_t* data = "Hello world!\n How are you doing Today? Wie geht es dir? Mir geht es gut! Der Test war erfolgreich!\0";
+    uint8_t* data = "Hello, world!\0";
 
-    writePDC(10, 2, distances, 100, data, testData, testWidth, testHeight);
+    writePDC(10, 2, distances, 15, data, testImg, testWidth, testHeight);
 
-    stbi_write_png("test.png", testWidth, testHeight, 3, testData, testWidth*3);
+    stbi_write_png("test.png", testWidth, testHeight, 3, testImg, testWidth*3);
 
-    free(testData);
+    uint8_t* recv_data = NULL;
+    unsigned int recv_data_size = 0;
+
+    readPDC(10, 2, distances, recv_data, &recv_data_size, testImg, testWidth, testHeight);
+
+    printf("Image contains %d bytes of data\n", recv_data_size);
+
+    free(testImg);
 
     return 0;
 }
@@ -96,29 +103,86 @@ int writePDC(int point_size, unsigned int distance_count, unsigned int* distance
         printf(" %d %u\n", *(data + dbyte), dbyte);
     }
 
+    // write the end 
+    fillRect(currentX, currentY, point_size*3, point_size, img, img_width, img_height, current_color, 3);
+
     return 0;
 }
 
 int readPDC(int point_size, unsigned int distance_count, unsigned int* distances, uint8_t* data, unsigned int* data_size, uint8_t* img, int img_width, int img_height) {
-    // we know that the first point is going to be black so we can skipp. start in the middel of the point 
-    int readX = point_size, readY = point_size / 2;
-    int current_distance = 0;
-    _Bool was_white = 0;
-    _Bool was_black = 0;
+    int readX = 0, readY = point_size / 2;
+    int messured_distance = 0;
+    int threshold = 50;
+    int last_color = 0;
+    int current_color = 0;
+
+    uint8_t current_byte = 0;
+
 
     // messure the distance
-    // TODO make it compare to all colors. And make it look for black enouth not just black
-    while (1)  {
-        current_distance ++;
+    for (int interartions = 0;;)  {
+        messured_distance++;
 
-        readY ++;
+        readX ++;
 
-        // check if the pixel is white. Do that becouse otherwise we could se the point that we left
-        // TODO make it check all values and look for white enouth not just white
-        if (*(img + xyToPixelCoords(readX, readY, img_width, 3)) == 255)
-            was_white = 1;
-        else if (*(img + xyToPixelCoords(readX, readY, img_width, 3)) == 0)
-            was_black = 1;
-    } ;
+        // check if we have to jump to a new line and reset x
+        if (readX + point_size > img_width) {
+            readY += point_size;
+            // no plus point size becouse we start at the edge of the image
+            readX = 0;
+            // also reset the distance
+            messured_distance = 0;
+            printf("newline\n");
+        }
 
+        // check what color the pixel has
+        // TODO make it check all values
+        if (*(img + xyToPixelCoords(readX, readY, img_width, 3)) >= 255 - threshold)
+            current_color = 1;
+        else if (*(img + xyToPixelCoords(readX, readY, img_width, 3)) <= threshold)
+            current_color = 0;
+
+        // if the last color was white and now its black we have a distance!
+        if (last_color == 1 && current_color == 0) {
+            messured_distance = messured_distance - point_size;
+            (*data_size)++;
+
+            // only increase interations when wehre actulay reading somthing 
+            interartions++;
+
+            unsigned int distance = 0;
+
+            // figure out what distance it should be
+            // set best distance diference to a big ass value so everything else is smaller 
+            for (int i = 0, bestdd = 2147483647; i < distance_count; i++) {
+                // compare the messured distance to every distance that we have and chose the one thats nearest
+                if (abs(messured_distance - (int)*(distances + i)) < bestdd) {
+                    bestdd = abs(messured_distance - (int)*(distances + i));
+                    distance = i;
+                }
+            }
+            printf("messured: %d closest: %u\n", messured_distance, distance);
+            
+            if (interartions % 8 == 0) {
+                printf("byte: %d i: %d\n", current_byte, interartions);
+                current_byte = 0;
+            }
+
+            // update the current byte TODO update for more than binary
+            current_byte = (current_byte << 1) | distance;
+
+            messured_distance = 0;
+        }
+
+        // A point distance code is terminated when a black point biger than 2 times point size is there then we reached the end
+        if (messured_distance >= 2*point_size && current_color == 0 && last_color == 0) 
+            break;
+
+        last_color = current_color;
+    };
+
+    // data size is in bytes but we saved it in bits
+    *data_size = *data_size/8;
+
+    return 0;
 }
